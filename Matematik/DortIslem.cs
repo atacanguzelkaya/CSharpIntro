1fbe96002c48eec43ee5f1ef6de29428a994588d
atacanguzelkaya <atacangzlky@gmail.com>
2023-11-08 15:49:32 +0300

    Add project files.

---
+++ Matematik/DortIslem.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Matematik
+{
+    public class DortIslem
+    {
+        // Topla(2,3)
+        public void Topla(int sayi, int sayi2) 
+        {
+            int toplam = sayi + sayi2;
+            Console.WriteLine("Sonuc: "+ toplam);
+
+        }
+    }
+}
